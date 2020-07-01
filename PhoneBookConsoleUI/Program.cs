2cd942df788af37cc2a9ec8b5f8c7c264b5fd4de
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-06-30 22:26:56 -0500

    Currently a big do while loop. Need to encapsulate it into a class with an execute method.

--- PhoneBookConsoleUI/Program.cs
+++ PhoneBookConsoleUI/Program.cs
@@ -5,8 +5,49 @@ namespace PhoneBookConsoleUI
     class Program
     {
         static void Main(string[] args)
-        {
-            
+        {            
+            ConsoleLogging.IntroText();
+
+            do
+            {
+                ConsoleLogging.WhichActionText();
+                var userChoice = Console.ReadKey();
+                Console.WriteLine();
+                switch (userChoice.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        Console.Clear();
+                        PhoneBook.CreateContact();
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        Console.Clear();
+                        PhoneBook.GetAllContacts();
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        Console.WriteLine();
+                        Console.WriteLine("You pressed 3");
+                        break;
+
+                    case ConsoleKey.D4:
+                    case ConsoleKey.NumPad4:
+                        int id = ConsoleLogging.GetContactToDelete();
+                        PhoneBook.DeleteContact(id);
+                        break;
+
+                    case ConsoleKey.D5:
+                    case ConsoleKey.NumPad5:
+                        Console.Clear();
+                        Console.WriteLine("Thank You!");
+                        Environment.Exit(0);
+                        break;
+                }
+
+            } while (true);
         }
     }
 }
