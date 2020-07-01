f25b0f2faaa6e1504bdcadcfca556a2d5fa87138
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-06-30 22:22:54 -0500

    Added ConsoleLogging class with methods for writing text to the screen

---
+++ PhoneBookConsoleUI/ConsoleLogging.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace PhoneBookConsoleUI
+{
+    internal class ConsoleLogging
+    {
+        internal static void IntroText()
+        {
+            Console.WriteLine("Welcome to your phone book!");
+            Console.WriteLine();
+            Console.WriteLine("What action would you like to perform?");
+            Console.WriteLine();
+        }
+
+        internal static void WhichActionText()
+        {
+            Console.WriteLine("1 - Create a contact");
+            Console.WriteLine("2 - Read all contacts");
+            Console.WriteLine("3 - Update a contact");
+            Console.WriteLine("4 - Delete a contact");            
+            Console.WriteLine("5 - End Application");            
+        }
+
+        internal static void PrintContactInfo(Contact c)
+        {
+            Console.WriteLine($"Name: {c.FullName} Number: {c.PhoneNumber} Email: {c.EmailAddress}" +
+                $" Address:{c.Address} Date of Birth:{c.DOB.ToShortDateString()}");
+        }
+
+        internal static void GetUserInformation(Contact c)
+        {
+            Console.WriteLine("Please enter the contacts first name.");
+            c.FirstName = Console.ReadLine();
+            Console.WriteLine("\nPlease enter their last name.");
+            c.LastName = Console.ReadLine();
+            Console.WriteLine("\nPlease enter their telephone number.");
+            c.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("\nPlease enter their email address.");
+            c.EmailAddress = Console.ReadLine();
+            Console.WriteLine("\nPlease enter their address.");
+            c.Address = Console.ReadLine();
+            Console.WriteLine("\nPlease enter their date of birth. ex: dd/mm/yyyy");
+            c.DOB = DateTime.Parse(Console.ReadLine());
+        }
+
+        internal static int GetContactToDelete()
+        {
+            Console.WriteLine("Which contact do you want to delete? Please enter the id!");
+            PhoneBook.GetAllContacts();
+
+            int id = int.Parse(Console.ReadLine());
+
+            return id;
+        }
+
+        internal static void DeleteContactErrorText()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("An error has occurred, cannot delete that contact. Try again!");
+            Console.ResetColor();
+        }
+    }
+}
