76239febc30838aaec83a647e4f03f6c15297673
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-06-30 22:24:10 -0500

    Added a Contact class to represent a contact in the phone book

--- PhoneBookConsoleUI/Contact.cs
+++ PhoneBookConsoleUI/Contact.cs
@@ -1,6 +1,15 @@
-﻿namespace PhoneBookConsoleUI
+﻿using System;
+
+namespace PhoneBookConsoleUI
 {
     public class Contact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public string Address { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DOB { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
