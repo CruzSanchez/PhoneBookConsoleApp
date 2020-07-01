d4452090527168ac916a8a61003953d428ec65ab
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-06-30 22:25:41 -0500

    Added PhoneBook class, has the Create Read and Delete methods. Need to implement the Update method

--- PhoneBookConsoleUI/PhoneBook.cs
+++ PhoneBookConsoleUI/PhoneBook.cs
@@ -1,11 +1,49 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PhoneBookConsoleUI
 {
-    class PhoneBook
+    static class PhoneBook
     {
-        public List<Contact> ContactList { get; set; }
+        public static Dictionary<int, Contact> ContactList { get; set; }
+        private static int _key = 0;
+
+        static PhoneBook()
+        {
+            ContactList = new Dictionary<int, Contact>();
+        }
+
+        internal static void CreateContact()
+        {
+            Contact contact = new Contact();
+
+            ConsoleLogging.GetUserInformation(contact);
+
+            ContactList.TryAdd(_key, contact);
+
+            _key++;
+        }
+
+        internal static void GetAllContacts()
+        {
+            foreach (var contact in ContactList)
+            {
+                ConsoleLogging.PrintContactInfo(contact.Value);
+            }
+        }
+
+        internal static void DeleteContact(int id)
+        {
+            if (ContactList.Remove(id))
+            {
+                Console.WriteLine("Contact removed!");
+            }
+            else
+            {
+                ConsoleLogging.DeleteContactErrorText();
+            }
+        }
     }
 }
