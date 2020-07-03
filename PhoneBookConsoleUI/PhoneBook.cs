3075fef9f3ed6f6d48622360ff98fdf1242ef1fd
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-07-03 00:30:41 -0500

    Implemented the Update method. All basic functionality is working. Add json file for save data next, with methods

--- PhoneBookConsoleUI/PhoneBook.cs
+++ PhoneBookConsoleUI/PhoneBook.cs
@@ -28,6 +28,15 @@ namespace PhoneBookConsoleUI
 
         internal static void GetAllContacts()
         {
+            if (ContactList.Count == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No contacts!!!");
+                Console.WriteLine();
+                Console.ResetColor();
+                return;
+            }
+
             var ordered = ContactList.OrderBy(x => x.Value.FirstName).ThenBy(x => x.Value.LastName);
             foreach (var contact in ordered)
             {
@@ -37,13 +46,87 @@ namespace PhoneBookConsoleUI
 
         internal static void DeleteContact(int id)
         {
-            if (ContactList.Remove(id))
+            if(id == 0)
+            {
+                return;
+            }
+            else if (ContactList.Remove(id))
             {
                 Console.WriteLine("Contact removed!");
             }
             else
             {
                 ConsoleLogging.DeleteContactErrorText();
+            }
+        }
+
+        internal static void UpdateContact(int id)
+        {
+            if(id == 0)
+            {
+                return;
+            }
+            else if (ContactList.ContainsKey(id))
+            {
+                do
+                {
+                    Console.Clear();
+
+                    Contact c = GetContactFromList(id);
+                    ConsoleLogging.PrintContactInfo(id, c);
+
+                    Console.WriteLine("What do you want to update?");
+                    string propertyToUpdate = Console.ReadLine();
+                    UpdateProperty(c, propertyToUpdate);                    
+
+                } while (ConsoleLogging.UpdateAgain());
+            }
+            else
+            {
+                ConsoleLogging.ContactDoesNotExist();
+            }
+        }
+
+        private static Contact GetContactFromList(int id)
+        {
+            Contact c;
+            while (!ContactList.TryGetValue(id, out c))
+            {
+                ConsoleLogging.ContactDoesNotExist();
+                id = ConsoleLogging.GetContactToUpdate();
+            }
+            return c;
+        }
+
+        private static void UpdateProperty(Contact c, string propertyToUpdate)
+        {
+            switch (propertyToUpdate.ToLower())
+            {
+                case "first name":
+                    c.FirstName = ConsoleLogging.GetContactInfo("new first name");
+                    break;
+                case "last name":
+                    c.LastName = ConsoleLogging.GetContactInfo("new last name");
+                    break;
+                case "phone number":
+                    c.PhoneNumber = ConsoleLogging.GetContactInfo("new phone number");
+                    break;
+                case "address":
+                    c.Address = ConsoleLogging.GetContactInfo("new address");
+                    break;
+                case "email address":
+                case "email":
+                    c.EmailAddress = ConsoleLogging.GetContactInfo("new email address");
+                    break;
+                case "dob":
+                case "birthday":
+                    c.DOB = ConsoleLogging.GetContactInfo();
+                    break;
+                default:
+                    ConsoleLogging.InvalidInputText();
+                    Console.Write("Please press enter . . . ");
+                    Console.ReadLine();
+                    break;
             }
         }
     }
