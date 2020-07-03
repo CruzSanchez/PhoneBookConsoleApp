3386a8770e0a9dd7dc483e6502d69cb735ac1972
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-07-03 00:26:36 -0500

    Put the application into an execute method. If the app has never been executed before it forces the user to add the first contact before anything else.

--- PhoneBookConsoleUI/Program.cs
+++ PhoneBookConsoleUI/Program.cs
@@ -5,11 +5,24 @@ namespace PhoneBookConsoleUI
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
             ConsoleLogging.IntroText();
 
+            if (PhoneBook.ContactList.Count == 0)
+            {
+                ConsoleLogging.FirstTimeText();
+                PhoneBook.CreateContact();
+            }
+
+            Execute();
+        }
+
+        private static void Execute()
+        {
+            int contactId;
             do
             {
+                Console.Clear();
                 ConsoleLogging.WhichActionText();
                 var userChoice = Console.ReadKey();
                 Console.WriteLine();
@@ -19,24 +32,30 @@ namespace PhoneBookConsoleUI
                     case ConsoleKey.NumPad1:
                         Console.Clear();
                         PhoneBook.CreateContact();
+                        ConsoleLogging.PressEnter();
                         break;
 
                     case ConsoleKey.D2:
                     case ConsoleKey.NumPad2:
                         Console.Clear();
                         PhoneBook.GetAllContacts();
+                        ConsoleLogging.PressEnter();
                         break;
 
                     case ConsoleKey.D3:
                     case ConsoleKey.NumPad3:
-                        Console.WriteLine();
-                        Console.WriteLine("You pressed 3");
+                        Console.Clear();
+                        contactId = ConsoleLogging.GetContactToUpdate();
+                        PhoneBook.UpdateContact(contactId);
+                        ConsoleLogging.PressEnter();
                         break;
 
                     case ConsoleKey.D4:
                     case ConsoleKey.NumPad4:
-                        int id = ConsoleLogging.GetContactToDelete();
-                        PhoneBook.DeleteContact(id);
+                        Console.Clear();
+                        contactId = ConsoleLogging.GetContactToDelete();
+                        PhoneBook.DeleteContact(contactId);
+                        ConsoleLogging.PressEnter();
                         break;
 
                     case ConsoleKey.D5:
