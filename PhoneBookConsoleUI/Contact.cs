3b36d769082a96787882bbed552173ef4bfa3aa6
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-07-03 00:27:25 -0500

    Not sure what changed here... probably nothing.

--- PhoneBookConsoleUI/Contact.cs
+++ PhoneBookConsoleUI/Contact.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace PhoneBookConsoleUI
 {
     public class Contact
-    {
+    {        
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string FullName => $"{FirstName} {LastName}";
