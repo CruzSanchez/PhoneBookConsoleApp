6f2931e0cbdcda03a7203331cab1d8468b8489ea
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-07-06 20:07:36 -0500

    Added Repo and IRepo for contacts. Methods implemented now to do the rest."

---
+++ PhoneBookDataAccessLibrary/ContactRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace PhoneBookDataAccessLibrary
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ContactRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<ContactModel> GetAllContacts()
+        {
+            return _conn.GetAll<ContactModel>();
+        }
+        public ContactModel GetContact(int id)
+        {
+            return _conn.Get<ContactModel>(id);
+        }
+        public void CreateContact(ContactModel contact)
+        {
+            _conn.Insert(contact);
+        }
+        public void UpdateContact(ContactModel contact)
+        {
+            _conn.Update(contact);
+        }
+        public void DeleteContact(ContactModel contact)
+        {
+            _conn.Delete(contact);
+        }
+    }
+}
---
+++ PhoneBookDataAccessLibrary/IContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PhoneBookDataAccessLibrary
+{
+    public interface IContactRepository
+    {
+        void CreateContact(ContactModel contact);
+        void DeleteContact(ContactModel contact);
+        IEnumerable<ContactModel> GetAllContacts();
+        ContactModel GetContact(int id);
+        void UpdateContact(ContactModel contact);
+    }
+}
